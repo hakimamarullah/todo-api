9121c9f6a46e5416312ba471eab033f1d667eb06
hakimamarullah <hakimaamarullah@gmail.com>
2022-01-30 11:44:05 +0700

    create and get all to do

--- controller/main_controller.js
+++ controller/main_controller.js
@@ -2,10 +2,11 @@ const db = require("../database/db")
 
 const getAllTodo = async (req, res) => {
     try {
-
+    	const data = await db("SELECT * FROM TODOS ORDER BY due_date ASC");
+    	res.status(200).json({ok: true, data: data.rows})
     }
     catch(err) {
-
+    	res.status(404).json({ok: false, msg:err.message})
     }
 
 }
@@ -40,9 +41,12 @@ const deleteTodo = async (req, res) => {
 const createTodo = async (req, res) => {
     try {
     	const {content, due_date} = req.body;
-    	const newTodo = await db("INSERT INTO todos (content, due_date) VALUES($1,$2)",
+    	if(!content || !due_date){
+    		throw new Error("Content and due_date can't be empty");
+    	}
+    	const {command, rowCount, rows} = await db("INSERT INTO todos (content, due_date) VALUES($1,$2) RETURNING content",
     		content, due_date)
-    	res.status(201).json({success: true, msg: "successfully added new to do"})
+    	res.status(201).json({success: true, msg: "successfully added new to do", result:{command, rowCount, rows}})
     }
     catch(err) {
     	res.status(400).json({success: false, msg: err.message})
--- database/db.js
+++ database/db.js
@@ -1,23 +1,23 @@
 const { Pool } = require("pg")
-// const db = new Pool({
-// 	host: 'ec2-184-73-243-101.compute-1.amazonaws.com',
-// 	user: 'rcjuistugeuwax',
-// 	password: 'c2c16ca09d95f5c8d88533aa72e4667bbee02cb694bf96195eb9fdcfa3fb5ce3',
-// 	port: 5432,
-// 	URI: 'postgres://rcjuistugeuwax:c2c16ca09d95f5c8d88533aa72e4667bbee02cb694bf96195eb9fdcfa3fb5ce3@ec2-184-73-243-101.compute-1.amazonaws.com:5432/d6bn5q5pkoqprg'
-// });
+/*	database: d6bn5q5pkoqprg
+	host: 'ec2-184-73-243-101.compute-1.amazonaws.com',
+	user: 'rcjuistugeuwax',
+	password: 'c2c16ca09d95f5c8d88533aa72e4667bbee02cb694bf96195eb9fdcfa3fb5ce3',
+	port: 5432,
+	URI: 'postgres://rcjuistugeuwax:c2c16ca09d95f5c8d88533aa72e4667bbee02cb694bf96195eb9fdcfa3fb5ce3@ec2-184-73-243-101.compute-1.amazonaws.com:5432/d6bn5q5pkoqprg'
+*/
+
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL ?? 'postgres://rcjuistugeuwax:c2c16ca09d95f5c8d88533aa72e4667bbee02cb694bf96195eb9fdcfa3fb5ce3@ec2-184-73-243-101.compute-1.amazonaws.com:5432/d6bn5q5pkoqprg',
     ssl: {
-        rejectUnauthorized: true
+        rejectUnauthorized: false
     }
 });
 
 const db = async (query, ...params)=>{
 	const client = await pool.connect();
 	const data = await client.query(query,params);
-	client.release();
 	return data;
 }
 module.exports = db;
